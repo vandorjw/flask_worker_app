d7922004e366ea0f78a64fb39184bff73e85a473
Joost VanDorp <joostvandorp@gmail.com>
2017-05-08 16:02:30 -0400

    fix submit

--- flask/app/public/views.py
+++ flask/app/public/views.py
@@ -1,25 +1,28 @@
 # -*- coding: utf-8 -*-
 """Public section"""
 from datetime import datetime
+import json
 import time
 from random import randint
 from flask import Blueprint
-from flask import app
-from flask.app import sqs
+from app.sqs import submit_to_sqs
 
 blueprint = Blueprint('public', __name__)
+
 
 @blueprint.route('/', methods=['GET', 'POST'])
 def home():
     return "Home"
 
 
-@blueprint.route('/submit', method=['GET', ])
+@blueprint.route('/submit', methods=['GET', ])
 def submit_to_queue():
+    from flask_app import app
     timestamp = datetime.now().timestamp()
     payload = {'data': 'test', 'timestamp': timestamp, }
     try:
-        submit_to_sqs(app.config['SQS_REGION'], app.config['SQS_QUEUE_NAME'], json.dumps(payload))
+        result = submit_to_sqs(app.config['SQS_REGION'], app.config['SQS_QUEUE_NAME'], json.dumps(payload))
+        print(result)
         return "success"
     except Exception as err:
         short_error = err.args[0]
@@ -33,4 +36,3 @@ def random_request():
     sleep_time = randint(60, 300)
     time.sleep(sleep_time)
     return "do work for {}".format(sleep_time)
-
